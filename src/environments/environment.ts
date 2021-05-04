9da7cb56e092d763796aa5f212a8a6824235da17
FriedrichBlack <vigruksik@gmail.com>
2021-05-04 13:00:05 +0400

    direction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxKey: 'pk.eyJ1IjoidmlncnVrc2lrIiwiYSI6ImNrbnNyNGp3NDA2aWgyb254MnlvOXZrZ2sifQ.T-q4AQe8Ns5dMb2FSHfObw'
 };
 
 /*
