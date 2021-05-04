76ec313ed2c24e4c019db1cf348ca36faaa27dd4
FriedrichBlack <vigruksik@gmail.com>
2021-05-04 13:00:41 +0400

    direction

---
+++ src/app/maps/maps.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {environment} from "../../environments/environment";
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+export class MapsComponent implements OnInit {
+
+  constructor() { }
+
+  map: mapboxgl.Map;
+
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapboxKey;
+
+    this.map = new mapboxgl.Map({
+      container: 'map-mapbox', // container id
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [37.6164917, 55.7528143], // starting position
+      zoom: 12
+    });
+
+  }
+
+}
