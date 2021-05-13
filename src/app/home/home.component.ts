8a63031d3f6d75338ee69c00e98866fba62cf4f3
OlegTselishchev <oleks63@mail.ru>
2021-05-13 18:06:49 +0400

    update notification service and home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {AuthService} from "../services/auth.service";
 import {OrderService} from "../services/order.service";
 import {Order} from "../model/order.model";
@@ -6,6 +6,11 @@ import {AddressService} from "../services/address.service";
 import {Address} from "../model/address.model";
 import {TypeCargo} from "../model/typeCargo.model";
 import {TypeCargoService} from "../services/typeCargo.service";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from "rxjs/operators";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
 import {NotificationService} from "../services/notification.service";
 
 @Component({
@@ -15,47 +20,227 @@ import {NotificationService} from "../services/notification.service";
 })
 export class HomeComponent implements OnInit {
 
+  displayedColumns: string[] = ['id', 'name', 'status','price','loc','dest','volume', 'weight'];
+  dataSource: any;
+
+  public pageSize = 1;
+
+  @ViewChild
+  (MatPaginator) paginator: MatPaginator;
+
   constructor(private authService: AuthService,
               private orderService: OrderService,
               private addressService: AddressService,
-              private typeService: TypeCargoService) { }
+              private typeService: TypeCargoService,
+              private notificationService: NotificationService) { }
 
   public orderList: Order[] = [];
-  public addressList: Address[] = [];
-  public typeCargoList: TypeCargo[] = [];
+  // public addressList: Address[] = [];
+  // public typeCargoList: TypeCargo[] = [];
 
-  locCity: string;
-  destCity: string;
+
+  public addressListDest: Address[] = [];
+  public addressListLoc: Address[] = [];
+  public typeList: TypeCargo[] = [];
+
+
+  dest: Address = new Address();
+  loc: Address = new Address();
+  type: TypeCargo = new TypeCargo();
   price: number;
-  type: string;
+
+  isLoaderAddress: boolean = false;
+  isLoaderType: boolean = false;
+  isLoaderOrder: boolean = false;
+  isMap: boolean = false;
+  keyMap: boolean = true;
+
+
+  controlDest = new FormControl();
+  controlLoc = new FormControl();
+  controlType = new FormControl();
+
+  filteredAddressDest: Observable<Address[]>;
+  filteredAddressLoc: Observable<Address[]>;
+  filteredType: Observable<TypeCargo[]>;
+
+
+  inputDest: string = '';
+  inputLoc: string = '';
+  inputType: string = '';
+
+  // locCity: string;
+  // destCity: string;
+  // price: number;
+  // type: string;
 
   ngOnInit(): void {
-    this.getOrders();
-    this.getAddress();
+    this.getAddressCityDistinct();
     this.getType();
+
+    this.filteredAddressDest = this.controlDest.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterDest(value))
+    );
+
+    this.filteredAddressLoc = this.controlLoc.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterLoc(value))
+    );
+
+    this.filteredType = this.controlType.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterType(value))
+    );
+
+
+    // this.getAddress();
+
   }
 
-  public get isLoggedIn(): boolean{
-    return this.authService.isAuthenticated()
+  // public get isLoggedIn(): boolean{
+  //   return this.authService.isAuthenticated()
+  // }
+
+  // public getAddress(): void{
+  //   this.addressService.getAddressAll().subscribe((data: Address[])=>{this.addressList = data;});
+  // }
+  //
+
+
+  // public getOrders(): void{
+  //   if(this.loc.addressId != null && this.dest.addressId != null && this.type.typeId != null && this.price != null) {
+  //     this.orderService.getOrderListByLocDestTypePrice(this.loc.addressId, this.dest.addressId, this.type.typeId, this.price)
+  //       .subscribe((data: Order[]) => {
+  //         this.orderList = data
+  //       },
+  //       error => {
+  //         alert('error get orders')
+  //       },
+  //       () => {
+  //         console.log('getOrders-OK')
+  //       });
+  //   } else {alert('error data xxxxxx')}
+  // }
+
+
+  private _filterDest(value: string): Address[] {
+    const filterValue = this._normalizeValue(value);
+    return this.addressListDest.filter(address => this._normalizeValue(address.city).includes(filterValue));
   }
 
-  public getAddress(): void{
-    this.addressService.getAddressAll().subscribe((data: Address[])=>{this.addressList = data;});
+  private _filterLoc(value: string): Address[] {
+    const filterValue = this._normalizeValue(value);
+    return this.addressListLoc.filter(address => this._normalizeValue(address.city).includes(filterValue));
+  }
+
+  private _filterType(value: string): TypeCargo[] {
+    const filterValue = this._normalizeValue(value);
+    return this.typeList.filter(type => this._normalizeValue(type.name).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+
+  getAddressCityDistinct(): void {
+    this.addressService.getAddressAllCityDistinct().subscribe((data: Address[]) => {
+        this.addressListDest = data;
+        this.addressListLoc = data;
+      },
+      ()=>{this.isLoaderAddress = false;},
+      ()=>{this.isLoaderAddress = true;});
   }
 
   public getType(): void{
-    this.typeService.getType().subscribe((data: TypeCargo[])=>{this.typeCargoList = data});
+    this.typeService.getType().subscribe((data: TypeCargo[])=>{this.typeList = data},
+      ()=>{},
+      ()=>{this.isLoaderType = true});
   }
 
-  public getOrders(): void{
-      this.orderService.getOrderList().subscribe((data: Order[]) => {
-          this.orderList = data
-        },
-        error => {
-          alert('error get orders')
-        },
-        () => {
-          console.log('getOrders-OK')
-        });
+  addDestination(dest: Address): void {
+    this.dest = dest;
   }
+
+  clearDest(): void {
+    if (this.inputDest == '') {
+      this.dest.addressId = null;
+    } else {
+      return;
+    }
+  }
+
+  addLocation(loc: Address): void {
+    this.loc = loc;
+  }
+
+  clearLoc(): void {
+    if (this.inputLoc == '') {
+      this.loc.addressId = null;
+    } else {
+      return;
+    }
+  }
+
+  addType(type: TypeCargo): void {
+    this.type = type;
+  }
+
+  clearType(): void {
+    if (this.inputType == '') {
+      this.type.typeId = null;
+    } else {
+      return;
+    }
+  }
+
+  fillTableOrder():void{
+    if(this.loc.city != null && this.dest.city != null && this.type.typeId != null && this.price != null) {
+      this.orderService.getOrderListByLocDestTypePrice(this.loc.city, this.dest.city, this.type.typeId, this.price)
+        .subscribe((result: Order[]) => {
+            let array = [];
+            result.forEach(function (item) {
+              array.push({
+                "id": item.id,
+                "name": item.name,
+                "status": item.status.name,
+                "price": item.price,
+                "loc": item.location.city,
+                "dest": item.destination.city,
+                "volume": item.box.volume.toFixed(4),
+                "weight": item.box.weight
+              });
+            })
+            this.dataSource = new MatTableDataSource<any>(array);
+            this.dataSource.paginator = this.paginator;
+            this.orderList = result;
+          }, () => {
+            this.isLoaderOrder = false
+          },
+          () => {
+            this.isLoaderOrder = true;
+          });
+    }else{this.notificationService.add('dataError');
+    setTimeout(()=>{this.notificationService.remove('dataError')}, 2000);}
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  mapOn():void{
+    this.isMap = true;
+    this.keyMap = false;
+  }
+
+  mapOff():void{
+    this.isMap = false;
+    this.keyMap = true;
+  }
+
 }
