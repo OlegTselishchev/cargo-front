ff74305e47714cc15ba0d7edac021a9a060f3403
OlegTselishchev <oleks63@mail.ru>
2021-05-21 14:40:27 +0400

    update1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxKey: 'pk.eyJ1IjoidmlncnVrc2lrIiwiYSI6ImNrbnNyNGp3NDA2aWgyb254MnlvOXZrZ2sifQ.T-q4AQe8Ns5dMb2FSHfObw'
 };
